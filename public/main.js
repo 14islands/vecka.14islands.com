5c58956bd4d1217bfad8cd4c4a0080ff2256544c
Hjörtur Hilmarsson <hjortureh@gmail.com>
2016-12-15 10:39:14 +0100

    Fix to lighthouse issue

--- public/main.js
+++ public/main.js
@@ -8,14 +8,3 @@ const componentLoader = new ComponentLoader({
   Weeks
 })
 componentLoader.scan()
-
-/*
-* Register service worker
-*/
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then((reg) => {
-    console.log('Service Worker register', reg)
-  }).catch((err) => {
-    console.log('Service Worker error', err)
-  })
-}
